a649606acd3997940ef7cf4a301a05d0f31c3b83
Scott <you@example.com>
2020-03-29 13:23:38 -0700

    code updated to 31 of 226

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,14 +1,47 @@
 import React from 'react';
 
-const title = 'React';
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+
+      <hr/>
+
+      {list.map(function(item) {
+        return (
+          <div key={item.objectID}>
+            <span>
+              <a target="_blank" href={item.url}>{item.title} </a>
+            </span>
+            <br /><span>{item.author} </span>
+            <br /><span>Comments: {item.num_comments} </span>
+            <br /><span>Points: {item.points} </span>
+            <br />
+          </div>
+        );
+      })}
     </div>
   );
 }
