02c864363ab9a6a02108edac6ebce983c0d42331
Scott <you@example.com>
2020-03-29 16:53:12 -0700

    move list out of global scope into app scope and use React props to pass the array to the list component

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,45 +1,50 @@
 import React from 'react';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 const App = () => {
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const handleChange = event => {
+    console.log(event.target.value);
+  };
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange} />
 
       <hr/>
 
-      <List />
+      <List list={stories} />
 
     </div>
   );
-}
+};
 
-const List = () => 
-  list.map(item => (
+const List = props => 
+  props.list.map(item => (
       <div key={item.objectID}>
         <span>
-          <a target="_blank" href={item.url}>{item.title}</a>
+          <a target="_blank" rel="noopener noreferrer" href={item.url}>{item.title}</a>
         </span>
         <br /><span>{item.author} </span>
         <br /><span>Comments: {item.num_comments} </span>
