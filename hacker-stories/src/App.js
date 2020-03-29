dbd202280f5fa09508a123867c4b154af6b771f0
Scott <you@example.com>
2020-03-29 12:35:44 -0700

    define react variable outside app component in app.js

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+
+const title = 'React';
+
 function App() {
-return (
-<div>
-<h1>Hello World</h1>
-</div>
-);
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
 }
+
 export default App;
