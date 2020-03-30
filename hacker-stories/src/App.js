72504e58fbadefa2273b084fc3c445840218f335
Scott <you@example.com>
2020-03-29 17:06:25 -0700

    update p tag to show state while typig in search field

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -21,8 +21,12 @@ const App = () => {
     },
   ];
 
+  const [searchTerm, setSearchTerm] = React.useState('');
+
   const handleChange = event => {
-    console.log(event.target.value);
+    // console.log(event.target.value);
+    setSearchTerm(event.target.value);
+
   };
 
   return (
@@ -31,6 +35,10 @@ const App = () => {
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={handleChange} />
+
+      <p>
+        Searching for: <strong>{searchTerm}</strong>
+      </p>
 
       <hr/>
 
