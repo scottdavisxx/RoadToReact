6d5e5df2177b2f141ff9bc81c969dfbdd3a19224
Scott <you@example.com>
2020-05-04 22:11:49 -0700

    Revert "try three versions of adding items to the list" This commit got rid of the comments and points that were added before This reverts commit 48ca54fcd268f68ae3e3952e12ee006b411f3db7.

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -60,18 +60,19 @@ const Search = ({search, onSearch }) => (
     </div>
   );
 
-    const List = ({ list }) =>
-      list.map(item => <Item key={item.objectID} item={item} />);
-    const Item = ({ item }) => (
-      <div>
+
+const List = props => 
+  props.list.map(item => (
+      <div key={item.objectID}>
         <span>
-          <a href={item.url}>{item.title}</a>
+          <a target="_blank" rel="noopener noreferrer" href={item.url}>{item.title}</a>
         </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
+        <br /><span>{item.author} </span>
+        <br /><span>Comments: {item.num_comments} </span>
+        <br /><span>Points: {item.points} </span>
+        <br />
+        <br />
       </div>
-);
-  
+  ));
 
 export default App;
