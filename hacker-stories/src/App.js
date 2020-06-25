165f582aa7a219013a948b2b8f3cfd0f2014ea42
scottdavisxx <scottdavisxx@gmail.com>
2020-06-24 22:12:17 -0700

    finish step 2 page 92

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -42,6 +42,10 @@ const useSemiPersistentState = (key, initialState) => {
 const storiesReducer = (state, action) => {
   if (action.type === 'SET_STORIES') {
     return action.payload;
+  } else if (action.type === 'REMOVE_STORY') {
+    return state.filter(
+      story => action.payload.objectID !== story.objectID
+    );
   } else {
     throw new Error();
   }
