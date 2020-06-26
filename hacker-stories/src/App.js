e4feb335db43bcdc641acdec3e21267e43d7bbee
scottdavisxx <scottdavisxx@gmail.com>
2020-06-25 21:40:48 -0700

    code through p99

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,32 +1,5 @@
 import React from 'react';
 
-const initialStories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-]
-
-const getAsyncStories = () => 
-  new Promise(resolve =>
-    setTimeout(
-      () => resolve({ data: {stories: initialStories}}), 
-      2000
-    )
-  );
-
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
     localStorage.getItem(key) || initialState
@@ -71,6 +44,8 @@ const storiesReducer = (state, action) => {
       throw new Error();
   }
 };
+// A
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState(
@@ -87,13 +62,13 @@ const App = () => {
   React.useEffect(() => {
     dispatchStories({ type: 'STORIES_FETCH_INIT'});
 
-    getAsyncStories()
+    fetch(`${API_ENDPOINT} react`) // B
+      .then(response => response.json()) // C
       .then(result => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.stories,
+          payload: result.hits, // D
         });
-      setIsLoading(false);
     })
     .catch(() => 
     dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
